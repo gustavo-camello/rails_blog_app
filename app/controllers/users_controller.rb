332b3269af1792d70a1bcc00c0f819d5354a166b
Gustavo Camello <gucamello@gmail.com>
2021-05-10 10:03:37 +0100

    Sessions functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   
   def index
     @users = User.all
@@ -6,7 +7,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
     # @articles = @user.articles.paginate(page: params[:page], per_page: 1)
   end
@@ -16,12 +16,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the blog #{@user.username}"
       redirect_to articles_path
     else
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account has been updated"
       redirect_to @user
@@ -43,6 +42,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 
